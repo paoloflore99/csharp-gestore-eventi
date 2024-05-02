fa56e24561b2c1c1c16f77f80ed4ebd2964266e6
paolo flore <paoloflore937@gmail.com>
2024-05-02 09:55:59 +0200

    classe evento

---
+++ GestoreEventi/Evento.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GestoreEventi
+{
+    internal class Evento
+    {
+        public string Titolo {  get; set; }
+        public int Data { get; set; }
+        public int CapacitaMassima { get; set; }
+        public int PostiDisponibboli { get; set;}
+    }
+}
