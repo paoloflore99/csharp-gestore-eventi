01352076f3ca458b05ef2222fed00e9fca6fc8df
paolo flore <paoloflore937@gmail.com>
2024-05-02 10:43:13 +0200

    costruttore fatto

--- GestoreEventi/Evento.cs
+++ GestoreEventi/Evento.cs
@@ -1,16 +1,65 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace GestoreEventi
 {
-    internal class Evento
+    public class Evento
     {
-        public string Titolo {  get; set; }
-        public int Data { get; set; }
-        public int CapacitaMassima { get; set; }
-        public int PostiDisponibboli { get; set;}
+        public string Titolo
+        {
+            get { return Titolo; }
+            set 
+            {
+                if (string.IsNullOrEmpty(value))
+                {
+                    throw new ArgumentNullException("titolo obligatorio");
+                }
+            }
+        }
+        public DateTime Data {
+            get {  return Data; }
+            set
+            {
+                if (value < DateTime.Today)
+                {
+                    throw new ArgumentException("la data deve partire dal 2024/05/02 e non prima");
+                }
+            }
+        }
+        public int CapacitaMassima
+        {
+            get {  return CapacitaMassima; }
+            set
+            {
+            if ( CapacitaMassima < 0 )
+                {
+                    throw new Exception("la capacita massima deve essere positiva ");
+                }
+            }         
+        }
+        public int Postiprenotati
+        {
+            get { return Postiprenotati; }
+            set
+            {
+                if (Postiprenotati < 0)
+                {
+                    throw new ArgumentException("i posti prenotati devono essere positivi ");
+                }
+            }
+        }
+
+        public Evento(string Titolo , DateTime Data , int CapacitaMassima , int Postiprenotati)
+        {
+            this.Titolo = Titolo;
+            this.Data = Data;
+            this.CapacitaMassima = CapacitaMassima ;
+            this.Postiprenotati = Postiprenotati;
+            Postiprenotati = 0;
+        }
     }
 }
