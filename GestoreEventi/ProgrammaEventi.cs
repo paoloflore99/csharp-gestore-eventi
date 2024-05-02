3639404e75bea6a904d2883e2ce55a1ad4dc93f8
paolo flore <paoloflore937@gmail.com>
2024-05-02 17:39:36 +0200

    lis eventi + data

--- GestoreEventi/ProgrammaEventi.cs
+++ GestoreEventi/ProgrammaEventi.cs
@@ -10,12 +10,34 @@ namespace GestoreEventi
     {
         public string Titolo;
 
-        private List<Evento> Eventi;
+        private List<Evento> eventi;
 
         public ProgrammaEventi(string titolo)
         {
             this.Titolo = titolo;
-            Eventi = new List<Evento>();
+            eventi = new List<Evento>();
+        }
+
+        public void AggiungiEvento(Evento eventiaggiunti)
+        {
+           eventi.Add(eventiaggiunti);
+        }
+
+
+
+        public List<Evento>EventiInData(DateTime data)
+        {
+            List<Evento> eventiInData = new List<Evento>();
+
+            foreach (var evento in eventi)
+            {
+                if (evento.Data.Date == data.Date)
+                {
+                    eventiInData.Add(evento);
+                }
+            }
+
+            return eventiInData;
         }
 
 
