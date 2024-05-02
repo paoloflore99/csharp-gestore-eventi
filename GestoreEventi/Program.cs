ab9c956124c3806621af0924aa308162c04b707d
paolo flore <paoloflore937@gmail.com>
2024-05-02 09:37:36 +0200

    Aggiungere i file di progetto.

---
+++ GestoreEventi/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace GestoreEventi
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
